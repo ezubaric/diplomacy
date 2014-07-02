a36d1f9423da63f4ccf96a4915b9d4b536e17bbb
Jordan Boyd-Graber <jordanbg@gmail.com>
2014-07-01 22:17:44 -0400

    first pass at data processing

--- data_processing/extract_results.py
+++ data_processing/extract_results.py
@@ -24,72 +24,90 @@ def movement_results_bodies(conn):
         yield {"game": gg, "text": ii, "subject": ss, "time": tt}
 
 def parse_move(order, assumed_country):
-	if any(order.startswith(x) for x in kADJECTIVES):
-		country, order = order.split(" ", 1)
-	else:
+    order = order.strip()
+    if any(order.startswith(x) for x in kADJECTIVES):
+        country, order = order.split(" ", 1)
+    else:
 		country = assumed_country
 
-	assert order.startswith("Fleet") or order.startswith("Army"), "Bad start of order: %s" % order
-	unit_type, order = order.split(" ")
+    order = order.strip()
+    assert order.startswith("Fleet") or order.startswith("Army"), \
+      "Bad start of order: %s" % order
+    unit_type, order = order.split(" ", 1)
 
-	if " -> " in order:
-		start, stop = order.split(" -> ")
-	else:
-		assert "HOLD" in order, "No hold or movement in %s" % order
-		start, hold = order.split(" ")
-		stop = start
+    if " -> " in order:
+        locations = order.split(" -> ")
+        # Need to handle arbitrary number of locations because of convoys
+        start = locations[0]
+        stop = locations[-1]
+    else:
+        if order.startswith("Army") or order.startswith("Fleet"):
+            unit_type, order = order.split(" ", 1)
+
+        if " " in order:
+            start, hold = order.split(" ", 1)
+        else:
+            start = order.strip()
+        stop = start
 
     return country, unit_type, start, stop
 
 def movement_tuples(message, row):
-	for line in message.split("\n"):
-		if ":" in line:
-			country, order = line.split(":", 1)
-			if not country in kCOUNTRIES:
-				continue
-			row["country"] = country
+    for line in message.split("\\n"):
+        print("Processing: %s" % line)
+        if line.strip().startswith("Ownership of supply centers:"):
+            break
+        if "build pending" in line:
+            break
+        if ":" in line:
+            country, order = line.split(":", 1)
+            if not country in kCOUNTRIES:
+                print("Skipping %s" % line)
+                continue
+            row["country"] = country
 
-			if "(*" in order:
-				order, result = order.split("(*")
-				row["result"] = result
-			else:
-				row["result"] = ""
+            if "(*" in order:
+                order, result = order.split("(*")
+                row["result"] = result
+            else:
+                row["result"] = ""
 
-			if "SUPPORT" in order:
-				row["order_type"] = "SUPPORT"
-				unit, order = order.split("SUPPORT", 1)
+            if "SUPPORT" in order:
+                row["order_type"] = "SUPPORT"
+                unit, order = order.split("SUPPORT", 1)
+                unit = unit.strip()
 
-				assert unit.startswith("Fleet") or order.startswith("Army"), "Bad start of order: %s" % order
-				unit_type, location = unit.split(" ")
-				row["unit_type"] = unit_type
-				row["start_location"] = location
-				row["end_location"] = location
+                assert unit.startswith("Fleet") or unit.startswith("Army"), "Bad start of order: %s" % order
+                unit_type, location = unit.split(" ", 1)
+                row["unit_type"] = unit_type
+                row["start_location"] = location
+                row["end_location"] = location
                 sup_country, sup_type, sup_start, sup_end = \
-				  parse_move(order, country)
-				row["support_country"] = sup_country
-				row["support_type"] = sup_type
-				row["support_start"] = sup_start
-				row["support_end"] = sup_end
-			elif "->" or "HOLD" in order:
-				move_country, unit_type, start, end = parse_move(order, country)
-				row["unit_type"] = unit_type
-				row["start_location"] = start
-				row["end_location"] = end
-				row["support_country"] = ""
-				row["support_type"] = ""
-				row["support_start"] = ""
-				row["support_end"] = ""
-			else:
-				continue
-			yield row
+                  parse_move(order, country)
+                row["support_country"] = sup_country
+                row["support_type"] = sup_type
+                row["support_start"] = sup_start
+                row["support_end"] = sup_end
+            elif "->" or "HOLD" in order:
+                move_country, unit_type, start, end = parse_move(order, country)
+                row["unit_type"] = unit_type
+                row["start_location"] = start
+                row["end_location"] = end
+                row["support_country"] = ""
+                row["support_type"] = ""
+                row["support_start"] = ""
+                row["support_end"] = ""
+            else:
+                continue
+            yield row
 
 if __name__ == "__main__":
     conn = sqlite3.connect(sys.argv[1])
 
     for mm in movement_results_bodies(conn):
-		base_row = {}
-		base_row["game"] = mm["game"]
-		base_row["time"] = mm["time"]
-		base_row["subject"] = mm["subject"]
-        for rr in movement_tuples(mm["text"]):
-			print(rr)
+        base_row = {}
+        base_row["game"] = mm["game"]
+        base_row["time"] = mm["time"]
+        base_row["subject"] = mm["subject"]
+        for rr in movement_tuples(mm["text"], base_row):
+            print(rr)
