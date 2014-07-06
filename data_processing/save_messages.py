ff770606d5803cb361288df543aef7cd16d940f0
Srijan Kumar <srijankedia@gmail.com>
2014-07-06 00:04:22 -0400

    Saving only non-zero press standard games

--- data_processing/save_messages.py
+++ data_processing/save_messages.py
@@ -4,6 +4,7 @@ import csv
 import data_processing
 from data_processing.common import all_gamenames_standard, generate_game_presses
 import codecs
+import os
 
 if __name__ == "__main__":
     conn = sqlite3.connect(sys.argv[1])
@@ -14,6 +15,10 @@ if __name__ == "__main__":
             continue
         f = codecs.open(("./press_data/" + game.replace('/','-')).encode("ascii"),"w",encoding='utf8')
         f.write("sender,receivers,milestone,timestamp,message\n");
+        flag = 1
         for sender, receivers, milestone, sent, msg in generate_game_presses(conn, game):
+            flag = 0
             f.write(sender + "," + receivers + "," + milestone + "," +  sent + "," + msg.strip() + "\n")
         f.close()
+        if flag == 1:
+            os.remove(("./press_data/" + game.replace('/','-')).encode("ascii"))
