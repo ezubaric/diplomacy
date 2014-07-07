abb1b482e82459e37c2f086d86e86d6f1bc4df7a
Srijan Kumar <srijankedia@gmail.com>
2014-07-07 15:21:18 -0400

    Removed 'Re-' from sender/receiver name

--- data_processing/deduplicate.py
+++ data_processing/deduplicate.py
@@ -20,7 +20,7 @@ for f in tqdm(glob("{}/*".format(sys.argv[1]))):
         toRemove = []
         for fro_other, to_other, mile_other, time_other, msg_other in presses:
             if fro_other.startswith("Re-") and not fro.startswith("Re-") and msg_other == msg and mile_other == mile:
-                toRemove.append((fro_other, to_other, mile_other, time_other, msg_other))
+                toRemove.append([fro_other, to_other, mile_other, time_other, msg_other])
             elif not fro_other.startswith("Re-") and fro.startswith("Re-") and msg_other == msg and mile_other == mile:
                 exists = True
                 break
@@ -31,7 +31,12 @@ for f in tqdm(glob("{}/*".format(sys.argv[1]))):
         for item in toRemove:
             presses.remove(item)
         if not exists:
-            presses.append((fro, to, mile, time, msg))
+            presses.append([fro, to, mile, time, msg])
     wtr = unicodecsv.writer(open(f, "wb"), lineterminator="\n")
     wtr.writerow(header)
-    wtr.writerows(presses)
+    for press in presses:
+        if press[0].startswith("Re-"):
+            press[0] = press[0][3:]
+        if press[1].startswith("Re-"):
+            press[1] = press[1][3:]
+        wtr.writerow(press)
