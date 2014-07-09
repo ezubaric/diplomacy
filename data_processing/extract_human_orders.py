97e2814e726ba66cba2eadc4e32a7933a50c3615
Vlad Niculae <vlad@vene.ro>
2014-07-09 19:00:20 +0200

    Add human syntax for orders from @ezubaric's sample

--- data_processing/extract_human_orders.py
+++ data_processing/extract_human_orders.py
@@ -19,6 +19,105 @@ f LYO m WES,
 f WES m Naf,
 f TYS m Tun, &
 a Pie s f Mar.
+
+HOL to BEL supported by RUH
+NTH to ENG to cut support
+BUR to MUN to cut support
+KIE to HEL
+
+CLY-LVP
+NWG-EDI
+FIN-NOR
+
+
+A UKR-MOS
+A SEV S A UKR-MOS
+A BOH-SIL
+A WAR S A BOH-SIL
+A GAL S A WAR
+A RUM-UKR
+A TYR S A MUN
+F TYS-LYO
+F ION-TYS
+
+
+A BUR-RUH
+A MUN S A BUR-RUH
+A GAS-BUR
+A MAR S A GAS-BUR
+F BRE-PIC
+A PAR S F BRE-PIC
+F MAO-BRE
+F SPA(SC)-MAO
+F POR S F SPA(SC)-MAO
+F WES S F SPA(SC)-MAO
+A TUS-PIE
+
+Hel-Den
+Bel Holds
+Ruh S Bel
+Hol S Bel
+
+
+A GAL-SIL (leads the attack)
+A BOH S A GAL-SIL (supports the attack)
+A UKR-WAR (cuts support)
+A MOS S A UKR-WAR (just in case)
+A SEV S A MOS (keeps Moscow safe)
+A TYR S A MUN (protects Munich)
+A BUD-GAL
+A VIE S A BUD-GAL
+
+A MUN S A GAL-SIL (supports the attack)
+
+Spring:
+ A Tri-Ven
+ F TYS-Nap
+Also, since the German cant cover all my retreats:
+ A Tyr-Mun
+ A Boh S A Tyr-Mun
+
+I "block" your move to Naples
+ A Rom-Nap
+
+Fall :
+ A Ven-Tus (for convoy next year)
+ And possibly:
+ A Rom-Lon (or Bel, Wal, Lvp, or Cly) via convoy
+
+A GAL-WAR (leads the attack on Warsaw)
+A UKR S A GAL-WAR (supports the attack)
+A MOS S A GAL-WAR (supports the attack)
+A SEV S A MOS (supports Moscow)
+A TYR-MUN (stab #1!)
+A BOH S A TYR-MUN (supports stab #1)
+A VIE-GAL
+A TRI-VEN (stab #2!)
+F TYS-NAP (stab #3!)
+F LYO-WES
+
+A ROM-NAP
+
+I am
+planning to order A TYR-MUN, A BOH S A TYR-MUN.  Italy is expecting
+me to support Munich from Tyrolia, but he is also planning to order
+support from Burgundy, which is why I need your support from either
+Ruhr or Berlin for A TYR-MUN.
+
+Mao-eng
+Pic s bur
+
+a new alliance is formed
+a pic s bur
+a mar s bur
+a  bur hold
+f nth-den
+a pie-ven
+a apu s pie-ven
+f rom hold
+f ion s gre
+f aeg s bul
+f adr s pie-ven
 """  # TODO: get more
 
 territory = "[A-Z][A-Za-z]{2}"  # TODO: refine using list
