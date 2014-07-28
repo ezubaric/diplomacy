dc9eee92bcae72fc5dc7717c5a310e2be646f9f0
Vlad Niculae <vlad@vene.ro>
2014-07-28 17:49:57 +0200

    Plot evolution in all games

--- data_processing/plot_player_evolution.py
+++ data_processing/plot_player_evolution.py
@@ -2,6 +2,7 @@ import json
 from collections import defaultdict
 from matplotlib import pyplot as plt
 import seaborn as sns
+import numpy as np
 
 yearly_statuses = json.load(open("../data_processing/yearly_statuses.json"))
 
@@ -13,15 +14,22 @@ def game_visual_summary(gamename):
     for year, status in sorted_statuses:
         years.append(year)
         for country in countries:
-            evolutions[country].append(status.get(country, [0, 0])[0])
+            supplies, armies = status.get(country, [0, 0])
+            evolutions[country].append(supplies - armies)
+            # evolutions[country].append(np.sign(supplies - armies))
 
     for country in countries:
         plt.plot(evolutions[country], label=country)
-    plt.xticks(range(len(years)), years)
+    plt.xticks(range(len(years)), years, rotation=90)
     plt.legend()
-    plt.ylabel("Supply center owned")
+    plt.ylabel("supplies")
     plt.title(gamename)
-    plt.show()
 
 
-game_visual_summary('phan03')
+plt.figure(figsize=(20, 20))
+sz = np.ceil(np.sqrt(len(yearly_statuses)))
+for k, game in enumerate(yearly_statuses.keys()):
+    plt.subplot(8, 4, 1 + k)
+    game_visual_summary(game)
+plt.tight_layout()
+plt.savefig("player_evolutions.png")
